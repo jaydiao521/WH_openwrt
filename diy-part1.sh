6e40b5d6a57a0355230febea142a28b00fcce4a6
jaydiao521 <68695174+jaydiao521@users.noreply.github.com>
2021-01-23 02:15:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,7 @@
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '$a src-git diy https://github.com/jaydiao521/diy' feeds.conf.default
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone https://github.com/garypang13/luci-app-bypass.git package/lean/luci-app-bypass
