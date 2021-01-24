91672a87e3e8e1c99e512e9754c6f42d4af51472
jaydiao521 <68695174+jaydiao521@users.noreply.github.com>
2021-01-25 01:27:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,7 +13,6 @@
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-sed -i '$a src-git diy https://github.com/jaydiao521/diy' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld
