59dcbfce4d89e4829e1fd2ae6b60e0ff8b5ef6c7
jaydiao521 <68695174+jaydiao521@users.noreply.github.com>
2021-01-29 15:09:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,7 +18,7 @@ sed -i "2i # network config" ./package/lean/default-settings/files/zzz-default-s
 sed -i "3i uci set network.lan.ipaddr='192.168.10.11'" ./package/lean/default-settings/files/zzz-default-settings
 sed -i "4i uci set network.lan.proto='static'" ./package/lean/default-settings/files/zzz-default-settings # 静态 IP
 sed -i "5i uci set network.lan.type='bridge'" ./package/lean/default-settings/files/zzz-default-settings  # 接口类型：桥接
-sed -i "6i uci set network.lan.ifname='eth0'" ./package/lean/default-settings/files/zzz-default-settings  # 网络端口：默认 eth0，第一个接口
+sed -i "6i uci set network.lan.ifname='eth0 eth1 eth2 eth3 eth4'" ./package/lean/default-settings/files/zzz-default-settings  # 网络端口：默认 eth0，第一个接口
 sed -i "7i uci set network.lan.netmask='255.255.255.0'" ./package/lean/default-settings/files/zzz-default-settings    # 子网掩码
 sed -i "8i uci set network.lan.gateway='192.168.10.10'" ./package/lean/default-settings/files/zzz-default-settings  # 默认网关地址（主路由 IP）
 sed -i "9i uci set network.lan.dns='222.85.85.85'" ./package/lean/default-settings/files/zzz-default-settings  # 默认上游 DNS 地址
@@ -26,3 +26,5 @@ sed -i "10i uci commit network\n" ./package/lean/default-settings/files/zzz-defa
 #
 sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='OpenWrt by Han '/g" ./package/lean/default-settings/files/zzz-default-settings
 sed -i "s/hostname='OpenWrt'/hostname='H.Wang'/g" ./package/base-files/files/bin/config_generate
+#
+rm -rf ./package/lean/luci-theme-argon
