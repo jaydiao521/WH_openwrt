aadf2039888ed5e8b5027c67c651fb3cdc82d14a
jaydiao521 <68695174+jaydiao521@users.noreply.github.com>
2021-09-24 02:45:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 git clone https://github.com/godros/luci-app-godproxy.git package/luci-app-godproxy
 #git clone https://github.com/garypang13/luci-app-bypass.git package/luci-app-bypass
