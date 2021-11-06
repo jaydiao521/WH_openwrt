ab86efe7fb4e50e912a58de3aaca76fe5a5d191c
jaydiao521 <68695174+jaydiao521@users.noreply.github.com>
2021-11-06 17:47:11 +0800

    Create li.sh

---
+++ li.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+# sed -i 's/192.168.1.1/192.168.10.11/g' package/base-files/files/bin/config_generate
+# 网络配置信息，将从 zzz-default-settings 文件的第2行开始添加
+sed -i "2i # network config" ./package/default-settings/files/zzz-default-settings
+# 默认 IP 地址，旁路由时不会和主路由的 192.168.1.1 冲突
+sed -i "3i uci set network.lan.ipaddr='192.168.10.11'" ./package/default-settings/files/zzz-default-settings
+sed -i "4i uci set network.lan.proto='static'" ./package/default-settings/files/zzz-default-settings # 静态 IP
+sed -i "5i uci set network.lan.type='bridge'" ./package/default-settings/files/zzz-default-settings  # 接口类型：桥接
+sed -i "6i uci set network.lan.ifname='eth0 eth1 eth2 eth3'" ./package/default-settings/files/zzz-default-settings  # 网络端口：默认 eth0，第一个接口
+sed -i "7i uci set network.lan.netmask='255.255.255.0'" ./package/default-settings/files/zzz-default-settings    # 子网掩码
+sed -i "8i uci set network.lan.gateway='192.168.10.10'" ./package/default-settings/files/zzz-default-settings  # 默认网关地址（主路由 IP）
+sed -i "9i uci set network.lan.dns='114.114.114.114 223.5.5.5'" ./package/default-settings/files/zzz-default-settings  # 默认上游 DNS 地址
+sed -i "10i uci commit network\n" ./package/default-settings/files/zzz-default-settings
+#
+sed -i "s/DISTRIB_DESCRIPTION='OpenWrt'/DISTRIB_DESCRIPTION='OpenWrt by Han'/g" ./package/default-settings/files/zzz-default-settings
+sed -i "s/hostname='OpenWrt'/hostname='H.Wang'/g" ./package/base-files/files/bin/config_generate
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/$1$15T8S9pF$4hfjTe2Q8IxjhD0F8gUlw.:18682:0:99999:7:::/g' package/default-settings/files/zzz-default-settings
+#find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
+#find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
+# sed -i 's/root::0:0:99999:7:::/root:$1$15T8S9pF$4hfjTe2Q8IxjhD0F8gUlw.:18682:0:99999:7:::/g' package/base-files/files/etc/shadow
+# sed -i '/shadow/d' package/lean/default-settings/files/zzz-default-settings
+#
+# rm -rf ./package/lean/luci-theme-argon
+# rm -rf ./package/lean/v2ray
+# rm -rf ./package/lean/v2ray-plugin
