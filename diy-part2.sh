18d2b84b97890e598e08b6ff19e1e679344f3b94
jaydiao521 <68695174+jaydiao521@users.noreply.github.com>
2022-06-30 22:49:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -29,6 +29,9 @@ sed -i "s/DISTRIB_DESCRIPTION='OpenWrt'/DISTRIB_DESCRIPTION='OpenWrt by Han'/g" 
 sed -i "s/hostname='OpenWrt'/hostname='H.W'/g" ./package/base-files/files/bin/config_generate
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/$1$15T8S9pF$4hfjTe2Q8IxjhD0F8gUlw.:18682:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
 sudo rm -rf ./feeds/packages/net/mosdns && cp -rf ./feeds/kenzo/mosdns ./feeds/packages/net/mosdns
+sed -i 's/mosdns-neo/mosdns/g' ./feeds/kenzo/luci-app-mosdns/Makefile
+sed -i 's/mosdns-neo/mosdns/g' ./feeds/kenzo/mosdns/Makefile
+sed -i 's/mosdns-neo/mosdns/g' ./feeds/packages/net/mosdns/Makefile
 #find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
 #find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
 # sed -i 's/root::0:0:99999:7:::/root:$1$15T8S9pF$4hfjTe2Q8IxjhD0F8gUlw.:18682:0:99999:7:::/g' package/base-files/files/etc/shadow
