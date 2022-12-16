1071375ea07bfbd40a23c16c9d13a61c17cbe5f0
jaydiao521 <68695174+jaydiao521@users.noreply.github.com>
2022-12-17 00:08:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,17 +21,17 @@ sed -i "5i uci set network.lan.type='bridge'" ./package/lean/default-settings/fi
 sed -i "6i uci set network.lan.ifname='eth0'" ./package/lean/default-settings/files/zzz-default-settings  # 网络端口：默认 eth0，第一个接口
 sed -i "7i uci set network.lan.netmask='255.255.255.0'" ./package/lean/default-settings/files/zzz-default-settings    # 子网掩码
 sed -i "8i uci set network.lan.gateway='192.168.10.10'" ./package/lean/default-settings/files/zzz-default-settings  # 默认网关地址（主路由 IP）
-sed -i "9i uci set network.lan.dns='119.29.29.29 223.5.5.5'" ./package/lean/default-settings/files/zzz-default-settings  # 默认上游 DNS 地址
+sed -i "9i uci set network.lan.dns='119.29.29.29 101.226.4.6'" ./package/lean/default-settings/files/zzz-default-settings  # 默认上游 DNS 地址
 sed -i "10i uci commit network\n" ./package/lean/default-settings/files/zzz-default-settings
 #
 # sed -i "s/LUCI_DEPENDS:=+mosdns_neo/LUCI_DEPENDS:=+mosdns/g" ./feeds/kenzo/luci-app-mosdns/Makefile
 sed -i "s/DISTRIB_DESCRIPTION='OpenWrt'/DISTRIB_DESCRIPTION='OpenWrt by Han'/g" ./package/lean/default-settings/files/zzz-default-settings
 sed -i "s/hostname='OpenWrt'/hostname='H.W'/g" ./package/base-files/files/bin/config_generate
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/$1$15T8S9pF$4hfjTe2Q8IxjhD0F8gUlw.:18682:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
-sudo rm -rf ./feeds/packages/net/mosdns && cp -rf ./feeds/kenzo/mosdns ./feeds/packages/net/mosdns
-sed -i 's/mosdns-neo/mosdns/g' ./feeds/kenzo/luci-app-mosdns/Makefile
-sed -i 's/mosdns-neo/mosdns/g' ./feeds/kenzo/mosdns/Makefile
-sed -i 's/mosdns-neo/mosdns/g' ./feeds/packages/net/mosdns/Makefile
+#sudo rm -rf ./feeds/packages/net/mosdns && cp -rf ./feeds/kenzo/mosdns ./feeds/packages/net/mosdns
+#sed -i 's/mosdns-neo/mosdns/g' ./feeds/kenzo/luci-app-mosdns/Makefile
+#sed -i 's/mosdns-neo/mosdns/g' ./feeds/kenzo/mosdns/Makefile
+#sed -i 's/mosdns-neo/mosdns/g' ./feeds/packages/net/mosdns/Makefile
 #find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
 #find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
 # sed -i 's/root::0:0:99999:7:::/root:$1$15T8S9pF$4hfjTe2Q8IxjhD0F8gUlw.:18682:0:99999:7:::/g' package/base-files/files/etc/shadow
